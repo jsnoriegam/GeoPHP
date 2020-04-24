01a17bfa9476547515a32042fd06b4c4cdc5dd88
swen100 <swen.zanon@geoglis.de>
2020-04-24 08:34:42 +0200

    add geos installation

--- tests/install_geos_extension.sh
+++ tests/install_geos_extension.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+set -ev
+
 # Install geos library dependencies
 sudo apt-get install -y libgeos-dev/stable
 
 # Build and install GEOS PHP extension
+cd $HOME
 sudo git clone https://git.osgeo.org/gogs/geos/php-geos.git
 cd php-geos
+chmod ugo+x autogen.sh
+./autogen.sh
 ./configure
 sudo make install
 
 # add to php.ini
 # echo "extension=geos.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 echo "extension=geos.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-
